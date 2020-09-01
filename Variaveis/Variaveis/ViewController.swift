4e4a6ed778645760617223a0619db4e89e728eaf
Junior Fernandes <juniorfernandes@MacBook-Pro-de-Junior.local>
2020-08-31 22:48:49 -0300

    Aula 31082020 - Array, dicionarios, e laço de repetição

--- Variaveis/Variaveis/ViewController.swift
+++ Variaveis/Variaveis/ViewController.swift
@@ -31,6 +31,8 @@ class ViewController: UIViewController {
         print(nomeCompleto)
         print(sobre)
         
+        let curriculo: String = "nome: \(nome) \(sobrenome), idade: \(idade)"
+        
         var carro: String? = "Civic"
         var moto: String?
         
@@ -54,6 +56,124 @@ class ViewController: UIViewController {
 //        O script abaixo verifica se existe algum valor na variavel, se tiver, a variavel _moto receberá o valor       contigo
         if let _moto = moto {
             print (_moto)
+        }
+        
+        print("WHILE==================")
+        var index: Int = 1
+        
+        while index <= 10 {
+            print("index: \(index)")
+            index = index + 1
+        }
+        
+        print(index)
+        
+        print("FOR===================")
+        
+        for value in 1...10 {
+            print("index: \(value)")
+        }
+        
+        var name1:String = "Junior"
+        var sobrenome1: String = "Fernandes"
+        var idade1: Int = 32
+        
+        var name2:String = "Fred"
+        var sobrenome2: String = "Fernandes"
+        var idade2: Int = 42
+        
+        var name3:String = "Alex"
+        var sobrenome3: String = "Fernandes"
+        var idade3: Int = 52
+        
+        var curriculo1: String = "Nome: \(name1) \(sobrenome1), idade \(idade1), Desenvolverdor iOS, jogador de Call of Duty"
+        var curriculo2: String = "Nome: \(name2) \(sobrenome2), idade \(idade2), Desenvolverdor iOS, jogador de Call of Duty"
+        var curriculo3: String = "Nome: \(name3) \(sobrenome3), idade \(idade3), Desenvolverdor iOS, jogador de Call of Duty"
+        
+        for value in 1...4 {
+            if value == 1{
+                print(curriculo1)
+            }else  if value == 2{
+                print(curriculo2)
+            }else{
+                print(curriculo3)
+            }
+        }
+        
+//        Dicionario
+        var dic1:[String:Any] = [
+            "nome":"Junior",
+            "sobrenome":"Fernandes",
+            "idade": 30,
+            "curriculo":"Curriculo de Junior Fernandes"
+        ]
+        
+        var dic2:[String:Any] = [
+            "nome":"Fred",
+            "sobrenome":"Fernandes",
+            "idade": 40,
+            "curriculo":"Curriculo de Junior Fernandes"
+        ]
+        
+        var dic3:[String:Any] = [
+            "nome":"Alex",
+            "sobrenome":"Fernandes",
+            "idade": 50,
+            "curriculo":"Curriculo de Junior Fernandes"
+        ]
+        
+        
+        print("=========== dics")
+        print(dic1["nome"] ?? "")
+        
+//      Para adicionar um chave
+        print("=========== dics - add/update key")
+        dic1["aluno"] = true
+        dic1.updateValue("civic", forKey: "veiculo")
+        print(dic1)
+        
+//      Removendo um key
+        dic1.removeValue(forKey: "aluno")
+        
+        print("=========== dics keys")
+//      Imprimindo as keys
+        print(dic1.keys)
+        
+//        FORMAS DE CRIAR ARRAY
+//        Quando criamos o array com o option, nao precisa colocar o tipo
+        var arrayAlunosOptional:[String]?
+        var arrayAlunosInicializadoVazio:[String] = []
+        var arrayAlunosInicializadoComValor:[String] = ["Felipe", "Patricia", "Rodrido"]
+        
+        print(arrayAlunosOptional)
+        arrayAlunosOptional?.append("Felipe")
+        arrayAlunosOptional = ["Felipe", "Caio", "Patricia"]
+        arrayAlunosOptional?.append("Rodrigo")
+        print(arrayAlunosOptional)
+        print(arrayAlunosOptional?.first)
+        print(arrayAlunosOptional?[0] ?? "")
+        print(arrayAlunosOptional?[3] ?? "")
+        print(arrayAlunosOptional?.last)
+        
+        arrayAlunosOptional?.insert("Andre", at: 0)
+        
+        arrayAlunosOptional?.remove(at: 2)
+        
+        arrayAlunosOptional?[0] = "Bruno"
+        print(arrayAlunosOptional)
+        
+        
+        print("==========Imprimindo dics com array")
+        var arrayDics:[[String:Any]] = [dic1, dic2, dic3]
+        arrayDics += [dic2, dic3]
+        for value in arrayDics {
+            print(value)
+        }
+        
+        print("===================================")
+        print("===================================")
+        for (index, element) in arrayDics.enumerated() {
+            print("Pessoa\(index)===> \n \(element)")
         }
         
         
